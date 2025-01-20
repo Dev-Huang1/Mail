d650a6dc00052aab2a53df531eb6b852a1a5547b
Evan Huang <huangshaoqi8888@gmail.com>
2025-01-20 11:05:44 +0800

    Update route.ts

--- app/api/verify-password/route.ts
+++ app/api/verify-password/route.ts
@@ -5,15 +5,15 @@ export async function POST(request: Request) {
   const { password } = await request.json()
 
   if (password === process.env.ACCESS_PASSWORD) {
-    // Set a cookie to remember the valid password
-    cookies().set('access_token', 'true', {
+    const response = NextResponse.json('OK', { status: 200 })
+    response.cookies.set('access_token', 'true', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      maxAge: 60 * 60 * 24, // 24 hours
+      maxAge: 5 * 60,
     })
 
-    return new NextResponse('OK', { status: 200 })
+    return response
   }
 
   return new NextResponse('Unauthorized', { status: 401 })
